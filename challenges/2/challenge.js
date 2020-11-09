d3ee76809d876856230b0051c6f9eb0d44ccdfe5
Edson Marques <edson_csl@hotmail.com>
2020-11-09 12:08:47 -0300

    Solution of challenge 2

--- challenges/2/challenge.js
+++ challenges/2/challenge.js
@@ -13,11 +13,19 @@
  *
  * Exemplo:
  * sumPrimes(15) deve retornar 41
- *
+ * Solução: Um for para passar do numero 0 até o número N passado por parametro, 
+ * utilizando a função isPrime que se retorna true o valor de 'i' é somado a variavel sum
  */
 
 const isPrime = require('../../utils/isPrime')
 
-const sumPrimes = n => {}
+const sumPrimes = n => {
+    let sum = 0
+    for(let i = 0; i <= n; i++ )
+        if(isPrime(i)){
+            sum += i           
+        }
+    return sum
+}
 
 module.exports = sumPrimes
