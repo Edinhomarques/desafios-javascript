c51ac809811ffe52765add35e9a5854f3d83b195
Edson Marques <edson_csl@hotmail.com>
2020-11-09 12:07:37 -0300

    solution of challenge 1

--- challenges/1/challenge.js
+++ challenges/1/challenge.js
@@ -6,9 +6,14 @@
  *
  * Você deve escrever uma função que some
  * todos os argumentos providos.
- *
+ * Solução: Usando a o spread operator para a função aceitar N argumentos e
+ *  função reduce para somar todos os argumentos passados
  */
 
-const sumArguments = arr => {}
+const sumArguments = (...arr) => {
+    return sumArgs = arr.reduce((acc, crr) => {
+        return acc + crr
+    })
+}
 
 module.exports = sumArguments
