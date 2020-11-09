8ae0d0490abdca79fa0ba83b0e906172084753f7
Edson Marques <edson_csl@hotmail.com>
2020-11-09 14:08:53 -0300

    challenge 5 solution

--- challenges/5/challenge.js
+++ challenges/5/challenge.js
@@ -27,11 +27,41 @@
  * Para fazer a request, use nossa função chamada api, na
  * forma api().then()...
  *
- */
+ * 
+ * Solução primeiro usando o setLoading para true, usei a api().then() para fazer a requisição
+ * e setando o dado da resposta usando a função setData()
+ * após o setData usei o setLoading(false) para fechar a o loading.
+ * para os tratamento de erro usei o cath para setar para true caso exista algum erro,
+ * se não existir erro uso o setError dentro do then com o valor false
+ * 
+ * - Uma outra forma de fazer seria usando Async Await criei a função "doRequestWithAsyncAwait" para exemplificar
+ * */
 
 const { setData, setError, setLoading } = require('../../utils/stateHandlers')
 const api = require('../../utils/api') // Promise
 
-const doRequest = () => {}
+const doRequest = () => {
+    setLoading(true)
+    api().then(responseData => {
+        setData(responseData)
+        setLoading(false)
+        setError(false)
+    }).catch(() => setError(true))
 
+}
+const doRequestWithAsyncAwait = async () => {
+    setLoading(true)
+    try {
+        const response = await api();
+        setData(response)
+        setLoading(false)
+        setError(false)
+        
+    } catch (error) {
+        setError(true)
+        
+    }
+
+}
+doRequestWithAsyncAwait()
 module.exports = doRequest
